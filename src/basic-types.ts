60d61b7f87a4d64e492ecb107b967f17bc980bd5
kazi-kabir <kazi.s221999@gmail.com>
2022-11-03 20:29:01 +1100

    more basic types

--- src/basic-types.ts
+++ src/basic-types.ts
@@ -2,3 +2,8 @@ let id: number = 10;
 let username: string = 'foo'
 let isComplete: boolean = true;
 
+let person: [string, number];
+person = ["Bob", 20];
+
+// union types
+let customerId: string | number;
---
+++ src/object-types.ts
@@ -0,0 +1,13 @@
+type Person = {
+   name: string,
+   age: number,
+   country: string, 
+}
+
+const human : Person = {
+    name: "Bob",
+    age: 30,
+    country: "Australia"
+}
+
+console.log(human.name)

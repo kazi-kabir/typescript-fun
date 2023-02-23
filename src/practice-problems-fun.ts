0767a9013dcdcb7ed0357bf7d44e120614a3d13a
kazi-kabir <kazi.s221999@gmail.com>
2023-02-23 19:38:08 +1100

    warmup

--- src/practice-problems-fun.ts
+++ src/practice-problems-fun.ts
@@ -4,3 +4,12 @@ let time = today.toDateString()
 console.log(time);
 
 
+//even or odd
+function isEvenOrOdd(num: number) {
+    if (num % 2 === 0) {
+        return true;
+    }
+    return false;
+}
+
+console.log(isEvenOrOdd(2));

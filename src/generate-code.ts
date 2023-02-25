86769d1745d8450bfd52b0c3727c61e5fb9b3a12
kazi-kabir <kazi.s221999@gmail.com>
2023-02-25 16:27:17 +1100

    add hash

--- src/generate-code.ts
+++ src/generate-code.ts
@@ -1,4 +1,5 @@
 // generate a unique 4 digit code, for my personal productivity
+// extend later, maybe port to bash
 
 let val = Math.floor(1000 + Math.random() * 9000);
-console.log(val);
+console.log("#"+val);

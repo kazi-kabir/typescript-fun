dceee279531b6e3a1846ee5088539fd8ea0d0990
kazi-kabir <kazi.s221999@gmail.com>
2023-03-13 23:05:22 +1100

    notes and stickies

--- src/url-shortener.ts
+++ src/url-shortener.ts
@@ -5,3 +5,8 @@ console.log(url.includes(cutOff));
 
 let updatedUrl = url.replace(cutOff, '');
 console.log(updatedUrl);
+
+//TODO
+// > port to different proj
+// > make a new repo for this project
+// > sign up chrome developer

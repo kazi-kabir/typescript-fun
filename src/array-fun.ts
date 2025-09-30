982bbcf0b78d99a9627a7c4121b626ce178bf8d3
kazi-kabir <117260990+kazi-kabir@users.noreply.github.com>
2025-09-30 23:30:16 +1000

    Update array-fun.ts
    This is so easy

--- src/array-fun.ts
+++ src/array-fun.ts
@@ -4,6 +4,4 @@ const calculateSumOfTheArray = (numArray : Array<number>) => {
     return numArray.reduce((a, b) => a + b);
 }
 
-let result = calculateSumOfTheArray(numArray);
-console.log(result);
-
+console.log(calculateSumOfTheArray(numArray););
